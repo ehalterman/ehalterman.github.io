ddc83c57ab2cdd387751ea9eff07af34c28d2347
Halterman-Elise <59671334+Halterman-Elise@users.noreply.github.com>
2020-02-08 23:38:38 -0700

    update

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -1,6 +1,18 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+function banner() {
+    var date = new Date();
+    if (date.getDay = 6){
+        document.getElementsByClassName("pancakes").style.display = "block";
+    }
+    else {
+        document.getElementsByClassName("pancakes").style.display = "none";
+    }
+
+}
+
 
 const options1 = {day:'numeric', month:'long', year:'numeric', hour:'numeric', minute:'numeric'};
 document.getElementById('lastmod').textContent = new Date(document.lastModified).toLocaleDateString('en-US', options1);
+
