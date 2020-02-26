9732519cabf828a157c237097f0b14a5753d57af
Halterman-Elise <59671334+Halterman-Elise@users.noreply.github.com>
2020-02-25 23:22:11 -0700

    update

--- lesson7/js/main.js
+++ lesson7/js/main.js
@@ -30,3 +30,11 @@ if('IntersectionObserver' in window) {
       loadImages(img);
     });
   }
+
+  function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+
+const options1 = {day:'numeric', month:'long', year:'numeric', hour:'numeric', minute:'numeric'};
+document.getElementById('lastmod').textContent = new Date(document.lastModified).toLocaleDateString('en-US', options1);
