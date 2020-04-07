3eea977235151a23dedc938438487a9bc3a3f6d3
Halterman-Elise <59671334+Halterman-Elise@users.noreply.github.com>
2020-04-06 23:32:31 -0600

    update

--- salmon-river-adventures/js/main.js
+++ salmon-river-adventures/js/main.js
@@ -1,3 +1,27 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
   }
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?zip=83549,us&units=imperial&APPID=8789dd92c7cf955322e78e6d6bb50d55"
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        const currentTemp = document.querySelector('temperature');
+        document.getElementById('temperature').textContent = jsObject.main.temp_max;
+
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+
+        document.getElementById('windspeed').textContent = jsObject.wind.speed;
+
+        var t, s, windChill;
+        s = parseInt(document.getElementById("windspeed").textContent);
+        t = parseInt(document.getElementById("temperature").textContent);
+        if (t <= 50 && s > 3){
+        windChill = Math.round(35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16)));
+        }
+        else {
+        windChill = t;
+        }
+        document.getElementById("windchill").innerHTML = windChill;
+    })
