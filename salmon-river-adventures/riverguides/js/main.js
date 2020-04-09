579be4dc9ceacb8a239f6ccd5007934411348046
Halterman-Elise <59671334+Halterman-Elise@users.noreply.github.com>
2020-04-08 21:08:32 -0600

    update

--- salmon-river-adventures/riverguides/js/main.js
+++ salmon-river-adventures/riverguides/js/main.js
@@ -1,3 +1,18 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
   }
+
+const requestURL = 'https://ehalterman.github.io/salmon-river-adventures/riverguides.json';
+
+fetch(requestURL)
+  .then(function (response){
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const guides = jsonObject['riverguides'];
+    for (let i = 0; i < guides.length; i++) {
+      let riverguide = document.createElement('div');
+      let name = document.createElement('h3')
+      let 
+    }
+  })
