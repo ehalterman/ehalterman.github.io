fc52de2714339da8c9cde1ce22219050532c3d65
Halterman-Elise <59671334+Halterman-Elise@users.noreply.github.com>
2020-04-08 21:52:04 -0600

    update

--- salmon-river-adventures/riverguides/js/main.js
+++ salmon-river-adventures/riverguides/js/main.js
@@ -9,10 +9,32 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
+    console.table(jsonObject);
     const guides = jsonObject['riverguides'];
-    for (let i = 0; i < guides.length; i++) {
-      let riverguide = document.createElement('div');
+    for (let i = 0; i < guides.length; i++ ) {
+      let riverguide = document.createElement('section');
       let name = document.createElement('h3')
-      let 
-    }
-  })
+      let image = document.createElement('img');
+      let certification = document.createElement('p');
+      let experience = document.createElement('p');
+      let email = document.createElement('p');
+      let biography = document.createElement('p');
+      if (guides[i]) {
+        name.textContent = guides[i].name;
+        image.setAttribute('src', "images/" + guides[i].img);
+        certification.textContent = "Certification Level: " + guides[i].certlvl;
+        experience.textContent = "Experience: " + guides[i].experience;
+        email.textContent = "Email: " + guides[i].email;
+        biography.textContent = "Biography: " + guides[i].bio;
+        riverguide.appendChild(name);
+        riverguide.appendChild(image);
+        riverguide.appendChild(certification);
+        riverguide.appendChild(experience);
+        riverguide.appendChild(email);
+        riverguide.appendChild(biography);
+        image.setAttribute('alt', guides[i].name);
+      }
+
+    document.querySelector('div.riverguide').appendChild(riverguide);
+  }
+  });
